6a423550dac5e61f844b57ea429e0c8b90728974
Jean Lucas <142547374+jean2001369@users.noreply.github.com>
2023-09-14 20:11:53 -0300

    Add files via upload

--- Pratica_02/src/Armazem/Main.java
+++ Pratica_02/src/Armazem/Main.java
@@ -2,6 +2,29 @@ package Armazem;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Estoque produto = new Estoque("Produto A", 100, 20);
+
+        produto.darBaixa(30);
+
+        // Verificar a quantidade atual após a baixa
+        System.out.println(produto.Mostra());
+
+        System.out.println(produto.isMostra);
+
+        System.out.println;
+
+// Verificar se precisa repor o estoque
+        if (produto.precisaRepor()) {
+         System.out.println(
+         System.out System "É necessário repor o estoque de " + produto.getNome());
+        }
+
+        else {
+            System.out.println(System"Não é necessário repor o estoque de " + produto.getNome());
+        }
     }
 }
+
+
+
+
